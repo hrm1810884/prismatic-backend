a1ff678ab6f3b28361b7fa6a101954867045622b
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-07-12 01:15:50 +0900

    update sql

--- migrations/2024-07-11-132532_prismatic_mysql_init/up.sql
+++ migrations/2024-07-11-132532_prismatic_mysql_init/up.sql
@@ -1,11 +1,13 @@
-CREATE TABLE diaries (
-    user_id CHAR(36) NOT NULL,
+CREATE TABLE user (
+    user_id VARCHAR(255) NOT NULL,
     human_diary TEXT,
-    AI_diary_1 TEXT,
-    AI_diary_2 TEXT,
-    AI_diary_3 TEXT,
-    AI_diary_4 TEXT,
-    isPublic BOOLEAN,
+    ai_diary_1 TEXT,
+    ai_diary_2 TEXT,
+    ai_diary_3 TEXT,
+    ai_diary_4 TEXT,
+    is_public BOOLEAN,
     favorite_id INT,
-    PRIMARY KEY (user_id)       
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id)
 );
